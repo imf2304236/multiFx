53d252ec81ad07e0bca0ebc331be3a8d1ea74a58
Ian F <ianmichael.fennie@haw-hamburg.de>
2020-03-03 10:26:34 +0100

    Add chorus to AudioSystem.h

--- AudioSystem.h
+++ AudioSystem.h
@@ -8,69 +8,85 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioInputI2S            audioIn;        //xy=124.666748046875,834.6666870117188
-AudioAmplifier           gainInL;        //xy=289.666748046875,820.6666870117188
-AudioAmplifier           gainInR;        //xy=289.666748046875,850.6666870117188
-AudioFilterStateVariable filterR;        //xy=413.66680908203125,1096.0001220703125
-AudioFilterStateVariable filterL;        //xy=491.00006103515625,142.6667022705078
-AudioMixer4              mixerFlangeInR;         //xy=665,1003.3333129882812
-AudioMixer4              mixerFlangeInL;         //xy=676.6666259765625,100
-AudioMixer4              mixerDelayInR;  //xy=715.0000610351562,1193.6666870117188
-AudioEffectFlange        flangeR;        //xy=786.6666870117188,1006.6668090820312
-AudioMixer4              mixerDelayInL;  //xy=839.6666870117188,241.99998474121094
-AudioEffectDelay         delayR;         //xy=897.0000610351562,1338.6666870117188
-AudioMixer4              mixerDelayOutR; //xy=914.0000610351562,1213.6666870117188
-AudioMixer4              mixerReverbInR; //xy=999.33349609375,1566.66650390625
-AudioEffectDelay         delayL;         //xy=1019.6666870117188,404.99998474121094
-AudioEffectFlange        flangeL;        //xy=1023.3335571289062,98.33332824707031
-AudioMixer4              mixerDelayOutL; //xy=1052.6666870117188,264.99998474121094
-AudioMixer4              mixerReverbInL; //xy=1137.6666870117188,626.9999847412109
-AudioEffectFreeverb      reverbR;        //xy=1196.0001220703125,1682.9998779296875
-AudioEffectFreeverb      reverbL;        //xy=1303.6666870117188,697.9999847412109
-AudioMixer4              mixerFxL;       //xy=1586.666748046875,696.6666870117188
-AudioMixer4              mixerFxR;       //xy=1627.3333333333335,1507.3333129882812
-AudioMixer4              mixerMasterL;   //xy=1784.666748046875,831.6666870117188
-AudioMixer4              mixerMasterR;   //xy=1785.666748046875,927.6666870117188
-AudioOutputI2S           audioOut;       //xy=1940.666748046875,881.6666870117188
+AudioInputI2S            audioIn;        //xy=138,750
+AudioAmplifier           gainInL;        //xy=303,736
+AudioAmplifier           gainInR;        //xy=303,766
+AudioFilterStateVariable filterL;        //xy=461,310
+AudioFilterStateVariable filterR;        //xy=513,1090
+AudioMixer4              mixerChorusInL;         //xy=670,92
+AudioMixer4              mixerChorusInR;         //xy=762.5,858.5
+AudioMixer4              mixerFlangeInR; //xy=765,997
+AudioMixer4              mixerDelayInR;  //xy=770,1265.7500610351562
+AudioMixer4              mixerDelayInL;  //xy=774.25,390.75
+AudioMixer4              mixerFlangeInL; //xy=788,250
+AudioMixer4              mixerReverbInR; //xy=784,1513.7500610351562
+AudioMixer4              mixerReverbInL; //xy=789.25,628
+AudioEffectChorus        chorusL;        //xy=876.5,94
+AudioEffectFlange        flangeR;        //xy=934,994
+AudioEffectChorus        chorusR;        //xy=962.75,862.75
+AudioEffectFreeverb      reverbL;        //xy=967.7500610351562,631.5
+AudioEffectDelay         delayL;         //xy=970.5,511.25
+AudioMixer4              mixerDelayOutL; //xy=976,411.25
+AudioEffectDelay         delayR;         //xy=999.5,1378.2500610351562
+AudioEffectFreeverb      reverbR;        //xy=1004,1524
+AudioEffectFlange        flangeL;        //xy=1014.25,249
+AudioMixer4              mixerDelayOutR; //xy=1014,1288.2500610351562
+AudioMixer4              mixerFxL;       //xy=1534.4999389648438,338.0000305175781
+AudioMixer4              mixerFxR;       //xy=1564,1159
+AudioMixer4              mixerMasterL;   //xy=1827.6666259765625,717.666748046875
+AudioMixer4              mixerMasterR;   //xy=1828.6666259765625,801.666748046875
+AudioOutputI2S           audioOut;       //xy=1983.6666259765625,767.666748046875
 AudioConnection          patchCord1(audioIn, 0, gainInL, 0);
 AudioConnection          patchCord2(audioIn, 1, gainInR, 0);
 AudioConnection          patchCord3(gainInL, 0, filterL, 0);
 AudioConnection          patchCord4(gainInL, 0, mixerMasterL, 0);
 AudioConnection          patchCord5(gainInR, 0, filterR, 0);
 AudioConnection          patchCord6(gainInR, 0, mixerMasterR, 0);
-AudioConnection          patchCord7(filterR, 2, mixerDelayInR, 0);
-AudioConnection          patchCord8(filterR, 2, mixerReverbInR, 0);
-AudioConnection          patchCord9(filterR, 2, mixerFlangeInR, 0);
-AudioConnection          patchCord10(filterL, 2, mixerDelayInL, 0);
-AudioConnection          patchCord11(filterL, 2, mixerReverbInL, 0);
-AudioConnection          patchCord12(filterL, 2, mixerFlangeInL, 0);
-AudioConnection          patchCord13(mixerFlangeInR, flangeR);
-AudioConnection          patchCord14(mixerFlangeInL, flangeL);
-AudioConnection          patchCord15(mixerDelayInR, 0, mixerDelayOutR, 0);
-AudioConnection          patchCord16(flangeR, 0, mixerDelayInR, 2);
-AudioConnection          patchCord17(flangeR, 0, mixerFxR, 1);
-AudioConnection          patchCord18(flangeR, 0, mixerReverbInR, 2);
+AudioConnection          patchCord7(filterL, 2, mixerChorusInL, 0);
+AudioConnection          patchCord8(filterL, 2, mixerFlangeInL, 0);
+AudioConnection          patchCord9(filterL, 2, mixerDelayInL, 0);
+AudioConnection          patchCord10(filterL, 2, mixerReverbInL, 0);
+AudioConnection          patchCord11(filterR, 2, mixerFlangeInR, 0);
+AudioConnection          patchCord12(filterR, 2, mixerDelayInR, 0);
+AudioConnection          patchCord13(filterR, 2, mixerReverbInR, 0);
+AudioConnection          patchCord14(filterR, 2, mixerChorusInR, 0);
+AudioConnection          patchCord15(mixerChorusInL, chorusL);
+AudioConnection          patchCord16(mixerChorusInR, chorusR);
+AudioConnection          patchCord17(mixerFlangeInR, flangeR);
+AudioConnection          patchCord18(mixerDelayInR, 0, mixerDelayOutR, 0);
 AudioConnection          patchCord19(mixerDelayInL, 0, mixerDelayOutL, 0);
-AudioConnection          patchCord20(delayR, 0, mixerDelayOutR, 1);
-AudioConnection          patchCord21(mixerDelayOutR, delayR);
-AudioConnection          patchCord22(mixerDelayOutR, 0, mixerFxR, 2);
-AudioConnection          patchCord23(mixerDelayOutR, 0, mixerReverbInR, 3);
-AudioConnection          patchCord24(mixerReverbInR, reverbR);
-AudioConnection          patchCord25(delayL, 0, mixerDelayOutL, 1);
-AudioConnection          patchCord26(flangeL, 0, mixerDelayInL, 2);
-AudioConnection          patchCord27(flangeL, 0, mixerReverbInL, 2);
-AudioConnection          patchCord28(flangeL, 0, mixerFxL, 1);
-AudioConnection          patchCord29(mixerDelayOutL, delayL);
-AudioConnection          patchCord30(mixerDelayOutL, 0, mixerFxL, 2);
-AudioConnection          patchCord31(mixerDelayOutL, 0, mixerReverbInL, 3);
-AudioConnection          patchCord32(mixerReverbInL, reverbL);
-AudioConnection          patchCord33(reverbR, 0, mixerFxR, 3);
+AudioConnection          patchCord20(mixerFlangeInL, flangeL);
+AudioConnection          patchCord21(mixerReverbInR, reverbR);
+AudioConnection          patchCord22(mixerReverbInL, reverbL);
+AudioConnection          patchCord23(chorusL, 0, mixerFlangeInL, 1);
+AudioConnection          patchCord24(chorusL, 0, mixerDelayInL, 1);
+AudioConnection          patchCord25(chorusL, 0, mixerReverbInL, 1);
+AudioConnection          patchCord26(chorusL, 0, mixerFxL, 0);
+AudioConnection          patchCord27(flangeR, 0, mixerDelayInR, 2);
+AudioConnection          patchCord28(flangeR, 0, mixerReverbInR, 2);
+AudioConnection          patchCord29(flangeR, 0, mixerFxR, 1);
+AudioConnection          patchCord30(chorusR, 0, mixerFlangeInR, 1);
+AudioConnection          patchCord31(chorusR, 0, mixerDelayInR, 1);
+AudioConnection          patchCord32(chorusR, 0, mixerReverbInR, 1);
+AudioConnection          patchCord33(chorusR, 0, mixerFxR, 0);
 AudioConnection          patchCord34(reverbL, 0, mixerFxL, 3);
-AudioConnection          patchCord35(mixerFxL, 0, mixerMasterL, 1);
-AudioConnection          patchCord36(mixerFxR, 0, mixerMasterR, 1);
-AudioConnection          patchCord37(mixerMasterL, 0, audioOut, 0);
-AudioConnection          patchCord38(mixerMasterR, 0, audioOut, 1);
-AudioControlSGTL5000     sgtl5000;       //xy=107.33343505859375,53.00004577636719
+AudioConnection          patchCord35(delayL, 0, mixerDelayOutL, 1);
+AudioConnection          patchCord36(mixerDelayOutL, delayL);
+AudioConnection          patchCord37(mixerDelayOutL, 0, mixerReverbInL, 3);
+AudioConnection          patchCord38(mixerDelayOutL, 0, mixerFxL, 2);
+AudioConnection          patchCord39(delayR, 0, mixerDelayOutR, 1);
+AudioConnection          patchCord40(reverbR, 0, mixerFxR, 3);
+AudioConnection          patchCord41(flangeL, 0, mixerDelayInL, 2);
+AudioConnection          patchCord42(flangeL, 0, mixerReverbInL, 2);
+AudioConnection          patchCord43(flangeL, 0, mixerFxL, 1);
+AudioConnection          patchCord44(mixerDelayOutR, delayR);
+AudioConnection          patchCord45(mixerDelayOutR, 0, mixerReverbInR, 3);
+AudioConnection          patchCord46(mixerDelayOutR, 0, mixerFxR, 2);
+AudioConnection          patchCord47(mixerFxL, 0, mixerMasterL, 1);
+AudioConnection          patchCord48(mixerFxR, 0, mixerMasterR, 1);
+AudioConnection          patchCord49(mixerMasterL, 0, audioOut, 0);
+AudioConnection          patchCord50(mixerMasterR, 0, audioOut, 1);
+AudioControlSGTL5000     sgtl5000;       //xy=99,55
 // GUItool: end automatically generated code
 
 
